bbcc8db0a92212b7ee9df9872694843b1604bed2
Lucas Alonso <lucas.alonso@optsol.com.br>
2022-05-19 09:21:30 -0300

    Service creation

--- src/app/components/service/chart-service.service.ts
+++ src/app/components/service/chart-service.service.ts
@@ -1,16 +1,54 @@
-import { Injectable } from '@angular/core';
+import { PieChartModel } from './../pie-chart/models/pie-chart.model';
+import { LineChartModel } from './../line-chart/models/line-chart.model';
+import { DoughnutChartModel } from './../doughnut-chart/models/doughnut-chart.model';
+import { BarChartModel } from './../bar-chart/models/bar-chart.model';
+import { EventEmitter, Injectable, Output } from '@angular/core';
+import * as sr from '@microsoft/signalr';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChartServiceService {
+  @Output() notification: EventEmitter<ChartModel> =
+    new EventEmitter<ChartModel>();
 
-  constructor() { }
+  public connection?: sr.HubConnection;
 
-
-  getRandomIntInclusive(min: number, max: number) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  private getUrl(): string {
+    return 'https://localhost:7108/chartHub';
   }
+
+  initializeConnection() {
+    this.connection = new sr.HubConnectionBuilder()
+      .configureLogging(sr.LogLevel.Information)
+      .withUrl(this.getUrl())
+      .withAutomaticReconnect()
+      .build();
+
+    this.connection
+      .start()
+      .then(function () {
+        console.log('SignalR Connected!');
+      })
+      .catch(function (err) {
+        return console.error(err.toString());
+      });
+
+    this.connection.on('UpdateChart', (chartModel: ChartModel) => {
+      this.notification.emit({ ...chartModel } as ChartModel);
+    });
+  }
+
+  closeConnection() {
+    if (this.connection?.state === sr.HubConnectionState.Connected) {
+      this.connection.stop();
+    }
+  }
+}
+
+export interface ChartModel {
+  barChart: BarChartModel;
+  doughnutChart: DoughnutChartModel;
+  lineChart: LineChartModel;
+  pieChart: PieChartModel;
 }
